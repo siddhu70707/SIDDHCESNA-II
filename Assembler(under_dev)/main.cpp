81a4adb4612365d4fe6fd3adc71f87e7674a1d31
siddhu70707 <siddhant.kushwaha70707@gmail.com>
2026-03-31 13:39:22 +0530

    bootloader and initial_syntax function

--- Assembler(under_dev)/main.cpp
+++ Assembler(under_dev)/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+
 using namespace std;
 
+void initial_command(){
+    cout << "=============================================================" << endl;
+    cout << "|                                                           |" << endl;
+    cout << "|      SIDDHCESNA-II 16-bit Assembler v1.0                  |" << endl;
+    cout << "|          Command-Line Interface (C++17)                   |" << endl;
+    cout << "|                                                           |" << endl;
+    cout << "=============================================================" << endl;
+    cout << endl;
+    cout << "Available commands:" << endl;
+    cout << "  assemble <file>  - Assemble .sc file" << endl;
+    cout << "  view hex         - Display machine code in hexadecimal" << endl;
+    cout << "  view bin         - Display machine code in binary" << endl;
+    cout << "  view const       - Display symbol table" << endl;
+    cout << "  help             - Show this help message" << endl;
+    cout << "  exit             - Exit the Assembler" << endl;
+    cout << endl;
+}
+
 int main(){
+    
+    initial_command();
+    // main loop
+    while(true){
+        cout << "> ";
+        
+    }
 
-    std::cout << "assembler in development" << endl;
     return 0;
-
 }
